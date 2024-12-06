bc657a5d7c8c40adc85a92652f5862dd965e9db2
Gil <gil@Gils-MacBook-Pro.local>
2024-12-06 12:14:24 +0200

    commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,7 +25,8 @@ export default function RootLayout({ children }) {
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="blue" />
       <link rel="manifest" href="/manifest.json" />
-      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+
 
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
